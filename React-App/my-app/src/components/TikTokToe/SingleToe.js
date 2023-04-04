5a5ca574e0eaa92022fb701cb98fad77610ebf34
Babu Miya Mohammad <moba0222@netcracker.com>
2023-04-04 15:03:10 +0530

    react-code-26

--- React-App/my-app/src/components/TikTokToe/SingleToe.js
+++ React-App/my-app/src/components/TikTokToe/SingleToe.js
@@ -1,6 +1,6 @@
 function SingleToe(props){
     return (
-        <button id = {props.id} key = {props.id} onClick={props.tickTock} className="m-4 p-3 border border-grey-400 shadow text-xl font-bold text-blue-600 w-12 h-12">{props.toe}</button>
+        <button id = {props.id} key = {props.id} onClick={props.tickTock} className=" p-2 px-5 py-2 border border-grey-400 shadow text-xl font-bold text-blue-600 w-12 h-12">{props.toe}</button>
     )
 }
 
--- React-App/my-app/src/components/TikTokToe/TikTokToe.js
+++ React-App/my-app/src/components/TikTokToe/TikTokToe.js
@@ -1,31 +1,38 @@
 import { useState } from "react";
 import SingleToe from "./SingleToe";
 
-function TikTokToe (){
-    const a = Array(3).fill("").map(x => Array(3).fill(""))
-    const [toe,setToe] = useState(a);
-    const [status,setStatus] = useState("");
-    function handleClick(i,j){
+function TikTokToe() {
+    const a = Array(3).fill("").map(x => Array(3).fill(""));
+    const [toe, setToe] = useState(a);
+    const [status, setStatus] = useState("");
+    function handleClick(i, j) {
         const toes = [...toe];
-        if(!toes[i][j]) {
-            if(!status){
+        let state = "";
+        if (!toes[i][j]) {
+            if (!status) {
                 setStatus("X");
+                state = "X";
             } else {
                 setStatus(status === "X" ? "O" : "X");
+                state = status === "X" ? "O" : "X";
             }
-            toes[i][j] = status;
+            toes[i][j] = state;
             setToe(toes);
         }
     }
-    let createStructure = ()=>{
+    const resetGame = ()=>{
+        setStatus("");
+        setToe(a);
+    }
+    const createStructure = () => {
         let mainPost = [];
-        for(let i = 0; i < a.length; i++){
+        for (let i = 0; i < a.length; i++) {
             let posts = [];
-            for(let j = 0; j < a.length; j++){
-                posts.push(<SingleToe id = {`${i}${j}`} key = {`${i}${j}`} tickTock = {handleClick.bind(this,i,j)} toe = {toe[i][j]}/>)
+            for (let j = 0; j < a.length; j++) {
+                posts.push(<SingleToe id={`${i}${j}`} key={`${i}${j}`} tickTock={handleClick.bind(this, i, j)} toe={toe[i][j]} />)
             }
             mainPost.push(
-                <div key = {`${i}`} className="flex">
+                <div key={`${i}`} className="flex">
                     {posts}
                 </div>
             )
@@ -33,8 +40,9 @@ function TikTokToe (){
         return mainPost;
     }
     return (
-        <div className="m-4 p-3 border border-grey-400 shadow w-fit">
-            {/* <div className="flex">
+        <>
+            <div className="m-4 p-3 border border-grey-400 shadow w-fit">
+                {/* <div className="flex">
                 <div onClick={tickTock} className="m-4 p-3 border border-grey-400 shadow text-xl font-bold text-blue-600">{toe}</div>
                 <div className="m-4 p-3 border border-grey-400 shadow text-xl font-bold text-blue-600">{toe}</div>
                 <div className="m-4 p-3 border border-grey-400 shadow text-xl font-bold text-blue-600">{toe}</div>
@@ -49,10 +57,17 @@ function TikTokToe (){
                 <div className="m-4 p-3 border border-grey-400 shadow text-xl font-bold text-blue-600">{toe}</div>
                 <div className="m-4 p-3 border border-grey-400 shadow text-xl font-bold text-blue-600">{toe}</div>
             </div> */}
-            <div>
-                {createStructure()}
+                <div>
+                    {createStructure()}
+                </div>
             </div>
-        </div>
+            <div>
+                <button onClick={resetGame} type="reset" className="px-5 py-2 bg-red-500 rounded-3xl text-white">
+                    Reset
+                </button>
+            </div>
+        </>
+
     )
 }
 
